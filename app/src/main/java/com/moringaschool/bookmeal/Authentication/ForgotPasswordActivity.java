07f708b457f3d1c8cd529d10effcb3ee2770f792
vokumu <victoria.okumu@student.moringaschool.com>
2021-10-11 08:39:10 +0300

    Forgot Password content

--- app/src/main/java/com/moringaschool/bookmeal/Authentication/ForgotPasswordActivity.java
+++ app/src/main/java/com/moringaschool/bookmeal/Authentication/ForgotPasswordActivity.java
@@ -2,15 +2,56 @@ package com.moringaschool.bookmeal.Authentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.moringaschool.bookmeal.R;
 
-public class ForgotPasswordActivity extends AppCompatActivity {
+public class ForgotPasswordActivity extends AppCompatActivity implements View.OnClickListener{
+    Button login,reset;
+    TextView email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+        login=findViewById(R.id.login);
+        reset=findViewById(R.id.reset);
+        login.setOnClickListener(this);
+        reset.setOnClickListener(this);
+        //email = findViewById(R.id.email);
+    }
+    public boolean validateEmail(){
+        String val=email.getText().toString();
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if (val.isEmpty()) {
+            email.setError("Field cannot be empty");
+            return false;
+        } else if (!val.matches(emailPattern)) {
+            email.setError("Invalid email address");
+            return false;
+        } else {
+            email.setError(null);
+            return true;
+        }
+
+    }
+    @Override
+    public void onClick(View view) {
+        if(view == login){
+            Intent intent = new Intent(ForgotPasswordActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
+        if(view == reset){
+            if (!validateEmail()) {
+                return;
+            }
+
+        }
+
     }
 }
