86c2d43f926fc9331e73910cb650685486279fc6
vokumu <victoria.okumu@student.moringaschool.com>
2021-10-11 07:12:37 +0300

    Added validation to the register fields

--- app/src/main/java/com/moringaschool/bookmeal/Authentication/RegisterActivity.java
+++ app/src/main/java/com/moringaschool/bookmeal/Authentication/RegisterActivity.java
@@ -7,10 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.textfield.TextInputLayout;
 import com.moringaschool.bookmeal.R;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 Button toLogin,toRegister;
+TextInputLayout fullName,email,phone,password,confirmPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,11 +22,99 @@ Button toLogin,toRegister;
         toRegister.setOnClickListener(this);
         toLogin.setOnClickListener(this);
 
+        fullName =findViewById(R.id.fullName);
+        email = findViewById(R.id.email);
+        phone = findViewById(R.id.phone);
+        password = findViewById(R.id.password);
+        confirmPassword = findViewById(R.id.confirmPassword);
+
+    }
+    public boolean validateName(){
+        String val=fullName.getEditText().getText().toString();
+        if(val.isEmpty()){
+            fullName.setError("Field cannot be empty");
+            return false;
+        }
+        else{
+            fullName.setError(null);
+            fullName.setErrorEnabled(false);
+            return true;
+        }
+
+    }
+    public boolean validateEmail(){
+        String val=email.getEditText().getText().toString();
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if (val.isEmpty()) {
+            email.setError("Field cannot be empty");
+            return false;
+        } else if (!val.matches(emailPattern)) {
+            email.setError("Invalid email address");
+            return false;
+        } else {
+            email.setError(null);
+            email.setErrorEnabled(false);
+            return true;
+        }
+
+    }
+    public boolean validatePhone(){
+        String val = phone.getEditText().getText().toString();
+
+        if (val.isEmpty()) {
+            phone.setError("Field cannot be empty");
+            return false;
+        } else {
+            phone.setError(null);
+            phone.setErrorEnabled(false);
+            return true;
+        }
+
+    }
+    public boolean validatePassword(){
+        String val = password.getEditText().getText().toString();
+        String val2 = confirmPassword.getEditText().getText().toString();
+        String passwordVal = "^" +
+                //"(?=.*[0-9])" +         //at least 1 digit
+                //"(?=.*[a-z])" +         //at least 1 lower case letter
+                //"(?=.*[A-Z])" +         //at least 1 upper case letter
+                "(?=.*[a-zA-Z])" +      //any letter
+                "(?=.*[@#$%^&+=])" +    //at least 1 special character
+                "(?=\\S+$)" +           //no white spaces
+                ".{4,}" +               //at least 4 characters
+                "$";
+
+        if (val.isEmpty()) {
+            password.setError("Field cannot be empty");
+            return false;
+        } else if(val2.isEmpty()){
+            password.setError("Field cannot be empty");
+            return false;
+        }
+        else if (!val.matches(passwordVal)) {
+            password.setError("Password is too weak");
+            return false;
+        }
+        else if(val != val2){
+            password.setError("Password do not match");
+            return false;
+        }
+
+        else {
+            password.setError(null);
+            password.setErrorEnabled(false);
+            return true;
+        }
+
     }
 
     @Override
     public void onClick(View view) {
         if(view == toRegister){
+            if (!validateName() | !validatePassword() | !validatePhone() | !validateEmail() | !validateName()) {
+                return;
+            }
 
         }
         if(view == toLogin){
