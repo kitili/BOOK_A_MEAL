1a529579996d43841c6dcd73d628686c9ef82011
vokumu <victoria.okumu@student.moringaschool.com>
2021-10-11 06:57:44 +0300

    Added intent to go to login on button click

--- app/src/main/java/com/moringaschool/bookmeal/Authentication/RegisterActivity.java
+++ app/src/main/java/com/moringaschool/bookmeal/Authentication/RegisterActivity.java
@@ -2,15 +2,36 @@ package com.moringaschool.bookmeal.Authentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.moringaschool.bookmeal.R;
 
-public class RegisterActivity extends AppCompatActivity {
-
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+Button toLogin,toRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        toRegister=findViewById(R.id.toRegister);
+        toLogin=findViewById(R.id.backLogin);
+        toRegister.setOnClickListener(this);
+        toLogin.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == toRegister){
+
+        }
+        if(view == toLogin){
+            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+            startActivity(intent);
+
+        }
+
     }
 }
