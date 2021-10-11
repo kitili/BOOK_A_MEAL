5683b6f8bcd17f197e7251b3785c6573190dda9d
vokumu <victoria.okumu@student.moringaschool.com>
2021-10-11 06:10:08 +0300

    Added the animation to the button,image and text

--- app/src/main/java/com/moringaschool/bookmeal/UI/SplashScreenActivity.java
+++ app/src/main/java/com/moringaschool/bookmeal/UI/SplashScreenActivity.java
@@ -4,15 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.moringaschool.bookmeal.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
+    Animation topAnim,bottomAnim;
+    ImageView image;
+    TextView logo;
+    Button getStarted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+
+        //Animation
+        topAnim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim= AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //Hooks
+        image=findViewById(R.id.image_logo);
+        logo=findViewById(R.id.splach_hero_text);
+        getStarted=findViewById(R.id.getStartedButton);
+        image.setAnimation(topAnim);
+        logo.setAnimation(topAnim);
+        getStarted.setAnimation(bottomAnim);
     }
 }
