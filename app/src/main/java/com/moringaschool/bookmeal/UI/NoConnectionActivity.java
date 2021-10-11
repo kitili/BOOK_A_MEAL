577553044afec7ba025d2cfc9fd0ad64e85c9966
vokumu <victoria.okumu@student.moringaschool.com>
2021-10-11 08:47:13 +0300

    Added the no connection layout

---
+++ app/src/main/java/com/moringaschool/bookmeal/UI/NoConnectionActivity.java
@@ -0,0 +1,14 @@
+package com.moringaschool.bookmeal.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoConnectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_connection);
+    }
+}
