c9f55fe5016b2b8b59b4feb2987426c491f5bc15
vokumu <victoria.okumu@student.moringaschool.com>
2021-10-11 08:18:10 +0300

    Redirect to main

--- app/src/main/java/com/moringaschool/bookmeal/UI/MainActivity.java
+++ app/src/main/java/com/moringaschool/bookmeal/UI/MainActivity.java
@@ -1,8 +1,10 @@
-package com.moringaschool.bookmeal;
+package com.moringaschool.bookmeal.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.moringaschool.bookmeal.R;
 
 public class MainActivity extends AppCompatActivity {
 
