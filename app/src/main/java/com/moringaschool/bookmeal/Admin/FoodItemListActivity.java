05f0c39c5943ac138513c00e1777d6e78c4578fe
vokumu <victoria.okumu@student.moringaschool.com>
2021-10-12 06:39:24 +0300

    Added Food item list

---
+++ app/src/main/java/com/moringaschool/bookmeal/Admin/FoodItemListActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.bookmeal.Admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.bookmeal.R;
+
+public class FoodItemListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_item_list);
+    }
+}
