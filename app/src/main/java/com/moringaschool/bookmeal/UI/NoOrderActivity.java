996678f2517e42e527f5479481ece2f8fbd5c2b9
vokumu <victoria.okumu@student.moringaschool.com>
2021-10-12 05:53:59 +0300

    Activity no order creation

---
+++ app/src/main/java/com/moringaschool/bookmeal/UI/NoOrderActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.bookmeal.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.bookmeal.R;
+
+public class NoOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_order);
+    }
+}
