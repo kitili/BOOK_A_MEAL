3823be2c26f6dddc9750b5adfd5dfc2194fb33b2
vokumu <victoria.okumu@student.moringaschool.com>
2021-10-13 09:58:42 +0300

    Added the back button on order history

--- app/src/main/java/com/moringaschool/bookmeal/UI/NoOrderActivity.java
+++ app/src/main/java/com/moringaschool/bookmeal/UI/NoOrderActivity.java
@@ -2,15 +2,27 @@ package com.moringaschool.bookmeal.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import com.moringaschool.bookmeal.Authentication.LoginActivity;
+import com.moringaschool.bookmeal.Authentication.RegisterActivity;
 import com.moringaschool.bookmeal.R;
 
-public class NoOrderActivity extends AppCompatActivity {
+public class NoOrderActivity extends AppCompatActivity{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_no_order);
+
+
     }
+
+
+
 }
--- app/src/main/java/com/moringaschool/bookmeal/UI/OrderHistoryActivity.java
+++ app/src/main/java/com/moringaschool/bookmeal/UI/OrderHistoryActivity.java
@@ -2,15 +2,31 @@ package com.moringaschool.bookmeal.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import com.moringaschool.bookmeal.Authentication.RegisterActivity;
 import com.moringaschool.bookmeal.R;
 
-public class OrderHistoryActivity extends AppCompatActivity {
-
+public class OrderHistoryActivity extends AppCompatActivity implements View.OnClickListener{
+    ImageView backhome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_history);
+        backhome=findViewById(R.id.back_btn);
+        backhome.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        if (view == backhome) {
+            onBackPressed();
+            //Intent intent = new Intent(OrderHistoryActivity.this, RegisterActivity.class);
+            //startActivity(intent);
+
+        }
     }
 }
