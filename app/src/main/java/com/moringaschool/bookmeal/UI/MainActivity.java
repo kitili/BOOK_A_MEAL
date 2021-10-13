b952a89db435376ac7330b6fcfb5e7fcfec17b25
vokumu <victoria.okumu@student.moringaschool.com>
2021-10-13 08:31:28 +0300

    Added the menu for user navigation

--- app/src/main/java/com/moringaschool/bookmeal/UI/MainActivity.java
+++ app/src/main/java/com/moringaschool/bookmeal/UI/MainActivity.java
@@ -1,16 +1,79 @@
 package com.moringaschool.bookmeal.UI;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationView;
 import com.moringaschool.bookmeal.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+   Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*-----------------------Hooks--------------------------*/
+        drawerLayout=findViewById(R.id.drawer_layout);
+        navigationView=findViewById(R.id.nav_view);
+        //textView=findViewById(R.id.textView);
+       toolbar=findViewById(R.id.toolbar);
+       /*----------------------------Tool Bar-----------------------*/
+       setSupportActionBar(toolbar);
+       /*---------------------------Navigation Menu---------------------*/
+
+        //hide or show items
+        Menu menu=navigationView.getMenu();
+        menu.findItem(R.id.nav_logout).setVisible(false);
+        menu.findItem(R.id.nav_profile).setVisible(false);
+
+       navigationView.bringToFront();
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
+    }
+    @Override
+    public void onBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else
+        {super.onBackPressed();
+        }
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                break;
+            case R.id.nav_orders:
+                intent=new Intent(MainActivity.this,OrderActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_history:
+                intent=new Intent(MainActivity.this,OrderHistoryActivity.class);
+                startActivity(intent);
+                break;
+
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
