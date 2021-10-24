0e8d6b2d04292f60109b7ab85e27a3c0245e9d9a
vokumu <victoria.okumu@student.moringaschool.com>
2021-10-24 15:30:41 +0300

    Added delete and add to cart fucntionality

--- app/src/main/java/com/moringaschool/bookmeal/UI/UserCartActivity.java
+++ app/src/main/java/com/moringaschool/bookmeal/UI/UserCartActivity.java
@@ -7,11 +7,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -19,6 +21,7 @@ import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.moringaschool.bookmeal.ApiClient;
+import com.moringaschool.bookmeal.Authentication.ProfileActivity;
 import com.moringaschool.bookmeal.Model.Order;
 import com.moringaschool.bookmeal.Model.OrderResponse;
 import com.moringaschool.bookmeal.Model.Orders;
@@ -37,10 +40,10 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class UserCartActivity extends AppCompatActivity {
+public class UserCartActivity extends AppCompatActivity implements View.OnClickListener {
 
-    String token,user_id;
-    TextView order_amount;
+    String token,user_id,orders_id;
+    TextView order_amount,order_id;
     RecyclerView mRecyclerView;
     RecyclerView rvFood;
     OrderUserAdapter orderAdapter;
@@ -49,21 +52,28 @@ public class UserCartActivity extends AppCompatActivity {
     SharedPreferences sharedpreferences;
     TextView mErrorTextView;
     OrderResponseAdapter.RecyclerViewClickListener listener;
-    ImageView backhome;
+    ImageView backhome,delete;
+    Button checkout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_cart);
         order_amount=findViewById(R.id.order_amount);
+        order_id=findViewById(R.id.order_id);
+        delete=findViewById(R.id.delete);
+        checkout=findViewById(R.id.checkout);
         rvFood=findViewById(R.id.orderList);
         ordersList=new ArrayList<>();
+        delete.setOnClickListener(this);
+        checkout.setOnClickListener(this);
+
         setMenuInfo();
     }
     private void setMenuInfo() {
-        SharedPreferences sharedPreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
-        token = sharedPreferences.getString("access_token", "");
-        user_id = sharedPreferences.getString("idKey", "");
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        token = sharedpreferences.getString("access_token", "");
+        user_id = sharedpreferences.getString("idKey", "");
         token = "Bearer " + token;
         Call<List<OrderResponse>> orderResponseCall= ApiClient.getService().orderResponses(token);
 
@@ -74,36 +84,35 @@ public class UserCartActivity extends AppCompatActivity {
 
                 if(response.isSuccessful()){
                     List<OrderResponse> orders=response.body();
-                    for(OrderResponse ord:orders){
-                        String TAG="this is the tag";
-                        String UserId=ord.getCustomer().getId();
-                        Boolean status= ord.getIsOpen();
-                        if(UserId.equals(user_id) && Boolean.TRUE.equals(status)){
-                            order_amount.setText(Integer.toString(ord.getTotalAmount()));
-                          for(Order item:ord.getOrders()){
-
-                              Log.e(TAG,item.getMenuItem());
-                          }
-
-                            ordersList  = ord.getOrders();
-                            setOnclickListener();
-                            OrderResponseAdapter orderAdapter = new OrderResponseAdapter((ArrayList<Order>) ordersList, (OrderResponseAdapter.RecyclerViewClickListener) listener,user_id);
-                            rvFood.setAdapter(orderAdapter);
-                            RecyclerView.LayoutManager layoutManager =
-                                    new LinearLayoutManager(UserCartActivity.this);
-                            rvFood.setLayoutManager(layoutManager);
-                            rvFood.setHasFixedSize(true);
-                            rvFood.setVisibility(View.VISIBLE);
-                        }
-
+                    if (orders.isEmpty()) {
+                       Intent intent = new Intent(UserCartActivity.this, NoOrderActivity.class);
+                        startActivity(intent);
                     }
-                   // Class<? extends List> restaurantsList = response.body().getClass();
-                   // Toast.makeText(UserCartActivity.this,"msg======="+restaurantsList, Toast.LENGTH_LONG).show();
+                    else {
+                        for (OrderResponse ord : orders) {
+                            String TAG = "this is the tag";
+                            String UserId = ord.getCustomer().getId();
+                            Boolean status = ord.getIsOpen();
+                            if (UserId.equals(user_id) && Boolean.TRUE.equals(status)) {
+                                order_amount.setText(Integer.toString(ord.getTotalAmount()));
+                                orders_id = ord.getId();
+                                ordersList = ord.getOrders();
+                                setOnclickListener();
+                                OrderResponseAdapter orderAdapter = new OrderResponseAdapter((ArrayList<Order>) ordersList, (OrderResponseAdapter.RecyclerViewClickListener) listener, user_id);
 
+                                if(orderAdapter.getItemCount()==0){
+                                    order_amount.setText("No items");
+                                }
+                                rvFood.setAdapter(orderAdapter);
+                                RecyclerView.LayoutManager layoutManager =
+                                        new LinearLayoutManager(UserCartActivity.this);
+                                rvFood.setLayoutManager(layoutManager);
+                                rvFood.setHasFixedSize(true);
+                                rvFood.setVisibility(View.VISIBLE);
+                            }
 
-
-
-
+                        }
+                    }
                 }
                 else{
                     try {
@@ -129,5 +138,51 @@ public class UserCartActivity extends AppCompatActivity {
 
     private void setOnclickListener() {
         listener = null;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == delete){
+            String TAG="yjid id the tag";
+            Log.e(TAG,token+"msq==================>"+orders_id);
+            Call<Void> deleteOrdercall= ApiClient.getService().deleteOrder(token,orders_id);
+            deleteOrdercall.enqueue(new Callback<Void>() {
+                @Override
+                public void onResponse(Call<Void> deleteOrdercall, Response<Void> response) {
+                    Toast.makeText(UserCartActivity.this,"The order was deleted successfully",Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(UserCartActivity.this, MainActivity.class));
+                    finish();
+
+                }
+
+                @Override
+                public void onFailure(Call<Void> deleteOrdercall, Throwable t) {
+                    String message=t.getLocalizedMessage();
+                    Toast.makeText(UserCartActivity.this,message,Toast.LENGTH_LONG).show();
+                }
+            });
+
+        }
+        if(view==checkout){
+            Call<Void> completeOrdercall= ApiClient.getService().completeOrder(token,orders_id);
+            completeOrdercall.enqueue(new Callback<Void>() {
+
+                @Override
+                public void onResponse(Call<Void> completeOrdercall, Response<Void> response) {
+                    Toast.makeText(UserCartActivity.this,"The order was completed successfully",Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(UserCartActivity.this, OrderCompleteActivity.class));
+                    finish();
+
+                }
+
+                @Override
+                public void onFailure(Call<Void> completeOrdercall, Throwable t) {
+                    String message=t.getLocalizedMessage();
+                    Toast.makeText(UserCartActivity.this,message,Toast.LENGTH_LONG).show();
+                }
+            });
+
+        }
+
     }
 }
